fe96c9a9e38ce6f661baa3e55f02afe28d0fe57f
Andre Gesser <andre.alfonsog@gmail.com>
2026-01-28 19:55:17 -0300

    feat: add no transactions component with HTML, SCSS, and integrate into home feature

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,19 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { TransactionType } from '../../shared/transaction/enums/transaction-types.enum';
 import { Transaction } from '../../shared/transaction/interfaces/transaction.interface';
 import { Balance } from './components/balance/balance';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
 })
 export class Home {
   transactions = signal<Transaction[]>([
-    { title: 'Salário', value: 200, type: TransactionType.INCOME },
-    { title: 'Conta de Luz', value: 100, type: TransactionType.OUTCOME },
-    { title: 'Conta de Água', value: 100, type: TransactionType.OUTCOME },
+    // { title: 'Salário', value: 200, type: TransactionType.INCOME },
+    // { title: 'Conta de Luz', value: 100, type: TransactionType.OUTCOME },
+    // { title: 'Conta de Água', value: 100, type: TransactionType.OUTCOME },
   ]);
 }
