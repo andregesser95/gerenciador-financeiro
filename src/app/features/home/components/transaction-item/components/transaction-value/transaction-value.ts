c4c61a9562b443a07dc282a2d1a7ccb27693724b
Andre Gesser <andre.alfonsog@gmail.com>
2026-01-28 19:48:19 -0300

    feat: add transaction value component with styling and integrate into transaction item

---
+++ src/app/features/home/components/transaction-item/components/transaction-value/transaction-value.ts
@@ -0,0 +1,23 @@
+import { Component, computed, input } from '@angular/core';
+import { TransactionType } from '../../../../../../shared/transaction/enums/transaction-types.enum';
+import { Transaction } from '../../../../../../shared/transaction/interfaces/transaction.interface';
+
+const CssClasses = {
+  [TransactionType.INCOME]: 'income',
+  [TransactionType.OUTCOME]: 'outcome',
+};
+
+@Component({
+  selector: 'app-transaction-value',
+  styleUrl: './transaction-value.scss',
+  imports: [],
+  host: {
+    '[class]': 'cssClass()',
+  },
+  template: `{{ transaction().value }}`,
+})
+export class TransactionValue {
+  transaction = input.required<Transaction>();
+
+  cssClass = computed(() => CssClasses[this.transaction()?.type]);
+}
--- src/app/features/home/components/transaction-item/transaction-item.ts
+++ src/app/features/home/components/transaction-item/transaction-item.ts
@@ -2,12 +2,13 @@ import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Transaction } from '../../../../shared/transaction/interfaces/transaction.interface';
+import { TransactionValue } from './components/transaction-value/transaction-value';
 
 @Component({
   selector: 'app-transaction-item',
   templateUrl: './transaction-item.html',
   styleUrl: './transaction-item.scss',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, TransactionValue],
 })
 export class TransactionItem {
   transaction = input.required<Transaction>();
